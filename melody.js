969cca12174cfc37e9ebeeb04279a64c04360b39
doroshenkoes@gmail.com <doroshenkoes@gmail.com>
2011-12-29 14:18:33 +0400

    initial

---
+++ melody.js
@@ -0,0 +1,22 @@
+;(function() {
+	var melody = '1|e|8,1|g|8,1|b|8,1|g|8,1|a|4,1|g|8,1|f#|8,1|b|4,1|a|4,1|e|2';
+	var scale = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
+	var bounces = $('.b-ball_bounce');
+	var playNote = function(str) {
+		var notes    = str.split(',');
+		var note     = notes.shift();
+		var parts    = note.split('|');
+		var octave   = parseInt(parts[0]);
+		var tone     = parts[1];
+		var duration = parseInt(parts[2]);
+		var index    = $.inArray(tone, scale) + 12 * octave;
+		var timeout  = 4000 / duration;
+
+		bounces.eq(index).trigger('mouseover');
+		window.setTimeout(function() {
+			playNote(notes.join(','));
+		}, timeout);
+	};
+
+	play(melody);
+})();
